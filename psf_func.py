2940b50e38c931bb666736bf00ee477fab228f2d
AngeloLF <angelo.lamure@gmail.com>
2025-04-25 22:24:59 +0200

    remove jax because we have numba

--- psf_func.py
+++ psf_func.py
@@ -1,7 +1,5 @@
 import numpy as np
 from numba import njit
-import jax
-import jax.numpy as jnp
 import hparameters
 from time import time
 
@@ -43,57 +41,4 @@ def moffat2d_jit(x, y, amplitude, x_c, y_c, gamma, alpha):
     norm = (np.pi * gamma * gamma) / (alpha - 1)
     a *= amplitude / norm
     return a
-
-
-
-@jax.jit
-def moffat2d_jax(x, y, amplitude, x_c, y_c, gamma, alpha):
-    xc = x - x_c
-    yc = y - y_c
-    rr_gg = (xc * xc + yc * yc) / (gamma * gamma)
-    a = (1 + rr_gg) ** -alpha
-    norm = (jnp.pi * gamma * gamma) / (alpha - 1)
-    a *= amplitude / norm
-    return a
-
-
-
-if __name__ == '__main__':
-
-    yy, xx = np.mgrid[:40, :40]
-    pixels = np.asarray([xx, yy])
-
-    x_c, y_c = hparameters.R0
-    ampli = 1.0
-    gamma = 3.0
-    alpha = 3.0
-
-    Xpix, Ypix = pixels
-
-    nb_loop = 100000
-
-    total = time()
-
-    times = np.zeros(nb_loop)
-    for i in range(nb_loop):
-        t0 = time()
-        moffat2d(Xpix, Ypix, ampli, x_c, y_c, gamma, alpha)
-        times[i] = (time() - t0) * 1e6
-    print(f"Numpy : {np.mean(times):.1f} ~ {np.std(times):.1f} µs")
-
-    times = np.zeros(nb_loop)
-    for i in range(nb_loop):
-        t0 = time()
-        moffat2d_jit(Xpix, Ypix, ampli, x_c, y_c, gamma, alpha)
-        times[i] = (time() - t0) * 1e6
-    print(f"Numba : {np.mean(times):.1f} ~ {np.std(times):.1f} µs")
-
-    times = np.zeros(nb_loop)
-    for i in range(nb_loop):
-        t0 = time()
-        moffat2d_jax(Xpix, Ypix, ampli, x_c, y_c, gamma, alpha)
-        times[i] = (time() - t0) * 1e6
-    print(f"JAX   : {np.mean(times):.1f} ~ {np.std(times):.1f} µs")
-
-    print(time()-total, ' s for total')
 
